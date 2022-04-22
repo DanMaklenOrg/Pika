2bb3a8ca64a7392aa57f884b1cdc44821423fc8c
Dan Maklen <danmaklen@gmail.com>
2022-04-22 10:24:38 +0200

    [Fix] Get Domain Profile

--- Service/Program.cs
+++ Service/Program.cs
@@ -17,10 +17,16 @@ builder.Services.Configure<ServiceConfig>(builder.Configuration);
 
 // Mapping Config
 TypeAdapterConfig.GlobalSettings.Default.EnumMappingStrategy(EnumMappingStrategy.ByName);
+
 TypeAdapterConfig<Guid, string>.ForType().MapWith(guid => guid.ToString("N"));
 TypeAdapterConfig<string, Guid>.ForType().MapWith(guidStr => string.IsNullOrWhiteSpace(guidStr) ? Guid.Empty : Guid.Parse(guidStr));
+
 TypeAdapterConfig<string, EntryDbModel>.ForType().MapWith(id => new EntryDbModel {Id = id.Adapt<Guid>()});
-TypeAdapterConfig<ObjectiveDto, ObjectiveDbModel>.ForType().Map(model => model.Entries, dto => dto.EntriesId);
+TypeAdapterConfig<EntryDbModel, string>.ForType().MapWith(entry => entry.Id.Adapt<string>());
+
+TypeAdapterConfig<ObjectiveDto, ObjectiveDbModel>.ForType()
+    .TwoWays()
+    .Map(model => model.Entries, dto => dto.EntriesId);
 
 // DB
 builder.Services.Configure<DatabaseConfig>(builder.Configuration.GetSection("database"));
