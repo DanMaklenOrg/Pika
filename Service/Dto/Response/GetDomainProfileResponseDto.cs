b549090ffb490040bbfad78c44cc3fbb685269e5
Omar Nasr <danmaklen@gmail.com>
2022-04-23 02:03:44 +0200

    Add Missing JsonPropertyName (#21)

--- Service/Dto/Response/GetDomainProfileResponseDto.cs
+++ Service/Dto/Response/GetDomainProfileResponseDto.cs
@@ -1,10 +1,13 @@
+using System.Text.Json.Serialization;
 using Pika.Service.Dto.Common;
 
 namespace Pika.Service.Dto.Response;
 
 public struct GetDomainProfileResponseDto
 {
+    [JsonPropertyName("root_entry")]
     public EntryDto RootEntry { get; set; }
 
+    [JsonPropertyName("projects")]
     public List<ProjectDto> Projects { get; set; }
 }
