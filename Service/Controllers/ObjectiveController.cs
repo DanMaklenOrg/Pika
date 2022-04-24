ca1956699ac29777eda331ce9960b5c95ffc10ad
Omar Nasr <danmaklen@gmail.com>
2022-04-24 06:48:31 +0200

    New API: Add Entries to Existing Objective (#22)

---
+++ Service/Controllers/ObjectiveController.cs
@@ -0,0 +1,36 @@
+using Mapster;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Pika.DataLayer;
+using Pika.DataLayer.Model;
+
+namespace Pika.Service.Controllers;
+
+[ApiController]
+[Route("api/objective")]
+public class ObjectiveController : ControllerBase
+{
+    private readonly PikaDataContext db;
+    private readonly IOptionsMonitor<ServiceConfig> config;
+
+    public ObjectiveController(PikaDataContext db, IOptionsMonitor<ServiceConfig> config)
+    {
+        this.db = db;
+        this.config = config;
+    }
+
+    [HttpPost("{objectiveId}/entry")]
+    public async Task<ActionResult> AddEntries(string objectiveId, [FromBody] List<string> entriesId, [FromHeader] string? authorization)
+    {
+        if (authorization != this.config.CurrentValue.Token) return this.Unauthorized();
+
+        var entries = entriesId.Adapt<List<EntryDbModel>>();
+        this.db.AttachRange(entries);
+        ObjectiveDbModel objective = await this.db.Objectives.SingleAsync(obj => obj.Id == objectiveId.Adapt<Guid>());
+        objective.Entries.AddRange(entries);
+        await this.db.SaveChangesAsync();
+
+        return this.Ok();
+    }
+}
