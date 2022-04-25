c97925c1eb51b58762b8c72f0a42e7b23686f752
Dan Maklen <danmaklen@gmail.com>
2022-04-25 08:15:54 +0200

    API: Add Objectives to Existing Project API

---
+++ Service/Controllers/ProjectController.cs
@@ -0,0 +1,39 @@
+using Mapster;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Pika.DataLayer;
+using Pika.DataLayer.Model;
+using Pika.Service.Dto.Common;
+
+namespace Pika.Service.Controllers;
+
+[ApiController]
+[Route("api/project")]
+public class ProjectController : ControllerBase
+{
+    private readonly PikaDataContext db;
+    private readonly IOptionsMonitor<ServiceConfig> config;
+
+    public ProjectController(PikaDataContext db, IOptionsMonitor<ServiceConfig> config)
+    {
+        this.db = db;
+        this.config = config;
+    }
+
+    [HttpPost("{projectId}/objective")]
+    public async Task<ActionResult> AddObjectives(string projectId, [FromBody] List<ObjectiveDto> objectives, [FromHeader] string? authorization)
+    {
+        if (authorization != this.config.CurrentValue.Token) return this.Unauthorized();
+
+        var objectiveDbModels = objectives.Adapt<List<ObjectiveDbModel>>();
+
+        this.db.AttachRange(objectiveDbModels.SelectMany(obj => obj.Entries));
+
+        ProjectDbModel project = await this.db.Projects.SingleAsync(proj => proj.Id == projectId.Adapt<Guid>());
+        project.Objectives.AddRange(objectiveDbModels);
+        await this.db.SaveChangesAsync();
+
+        return this.Ok();
+    }
+}
