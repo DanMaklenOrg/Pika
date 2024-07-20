4a8eaf2022f4036ef5aca81d8dfe18a1c6bf92ec
Dan Maklen <danmaklen@gmail.com>
2024-07-20 05:16:24 +0300

    Steam Scrapper

---
+++ DomainData/Scrapper/SteamScrapper.cs
@@ -0,0 +1,21 @@
+using Pika.Model;
+
+namespace Pika.DomainData.Scrapper;
+
+public class SteamScrapper(SteamClient client, EntityNameContainer nameContainer)
+{
+    public async Task<List<Entity>> ScrapAchievements(DomainId domainId, uint steamAppId)
+    {
+        var achievements = await client.GetAchievements(steamAppId);
+        return achievements.Select(a =>
+        {
+            var name = nameContainer.RegisterAndNormalize(a.DisplayName, "Achievement");
+            return new Entity
+            {
+                Id = ResourceId.InduceFromName(name, domainId),
+                Name = name,
+                Classes = ["_/achievement"],
+            };
+        }).ToList();
+    }
+}
