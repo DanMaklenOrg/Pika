3e11bba82feb3d8bbdf14452541fdee17603bd55
Dan Maklen <danmaklen@gmail.com>
2024-08-02 22:15:46 +0300

    Pika Data: Hades

--- DomainData/Program.cs
+++ DomainData/Program.cs
@@ -28,6 +28,7 @@ builder.Services.AddTransient<IScrapper, DungeonSoulsScrapper>();
 builder.Services.AddTransient<IScrapper, ShapezScrapper>();
 builder.Services.AddTransient<IScrapper, HadesScrapper>();
 builder.Services.AddTransient<IScrapper, HadesKeepsakes>();
+builder.Services.AddTransient<IScrapper, HadesFates>();
 
 builder.Services.AddTransient<SteamClient>();
 builder.Services.AddTransient<SteamScrapper>();
