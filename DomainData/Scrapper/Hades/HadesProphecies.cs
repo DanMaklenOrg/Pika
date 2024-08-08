14345807519762ce5d95ab465377d671ec926259
Omar Nasr <danmaklen@gmail.com>
2024-08-08 15:22:48 +0300

    Pika data hades (#88)

---
+++ DomainData/Scrapper/Hades/HadesProphecies.cs
@@ -0,0 +1,37 @@
+using HtmlAgilityPack;
+using Pika.Model;
+
+namespace Pika.DomainData.Scrapper.Hades;
+
+public class HadesProphecies(EntityNameContainer nameContainer) : IScrapper
+{
+    public DomainId DomainId => "hades";
+    public string OutputDirectory => DomainId.ToString();
+    public string FileName => "Prophecies";
+
+    public async Task<Domain> Scrape()
+    {
+        return new Domain
+        {
+            Id = DomainId,
+            Name = "Hades",
+            Entities = await ScrapeEntities(),
+        };
+    }
+
+    private async Task<List<Entity>> ScrapeEntities()
+    {
+        var doc = await new HtmlWeb().LoadFromWebAsync("https://hades.fandom.com/wiki/Fated_List_of_Minor_Prophecies");
+        var nodes = doc.DocumentNode.SelectNodes("//table[contains(@class, 'wikitable')]/tbody/tr/td[1]");
+        return nodes.Select(n =>
+        {
+            var name = nameContainer.RegisterAndNormalize(n.InnerText, "Fate");
+            return new Entity
+            {
+                Id = ResourceId.InduceFromName(name, DomainId),
+                Name = name,
+                Classes = [new ResourceId("prophecy", DomainId)],
+            };
+        }).ToList();
+    }
+}
---
+++ DomainData/Scrapper/Hades/HadesScrapper.cs
@@ -0,0 +1,22 @@
+using Pika.Model;
+
+namespace Pika.DomainData.Scrapper.Hades;
+
+public class HadesScrapper(SteamScrapper steamScrapper) : IScrapper
+{
+    private readonly uint _steamAppId = 1145360;
+
+    public DomainId DomainId => "hades";
+    public string OutputDirectory => DomainId.ToString();
+    public string FileName => DomainId.ToString();
+
+    public async Task<Domain> Scrape()
+    {
+        return new Domain
+        {
+            Id = DomainId,
+            Name = "Hades",
+            Entities = await steamScrapper.ScrapAchievements(DomainId, _steamAppId)
+        };
+    }
+}
