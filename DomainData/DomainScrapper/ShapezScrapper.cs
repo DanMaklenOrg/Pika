1981424efda84475ce48054fc5c56c3f74ecf966
Dan Maklen <danmaklen@gmail.com>
2024-08-10 01:11:51 +0300

    Update Shapez

--- DomainData/DomainScrapper/ShapezScrapper.cs
+++ DomainData/DomainScrapper/ShapezScrapper.cs
@@ -8,6 +8,7 @@ public class ShapezScrapper(SteamScrapperHelper steamScrapperHelper) : IScrapper
     private const uint SteamAppId = 1318690;
 
     public ResourceId DomainId => "shapez";
+
     public async Task ScrapeInto(Domain domain)
     {
         domain.Entities.AddRange(await steamScrapperHelper.ScrapAchievements(SteamAppId, "achievement"));
