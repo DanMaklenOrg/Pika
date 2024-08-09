8a309c5959221d902560a6add4592fa8d5563fba
Dan Maklen <danmaklen@gmail.com>
2024-08-09 13:52:15 +0300

    Remove Base Domain Dependencies: _/unlocked

--- DomainData/TestCommand.cs
+++ DomainData/TestCommand.cs
@@ -18,19 +18,63 @@ public static class TestCommand
     {
         await foreach (var domain in GetAllDomains(domainRepo))
         {
+            Console.WriteLine($"Testing {domain.Id} ({domain.Name})...");
             TestDomain(domain);
             var userStat = await userStatsRepo.Get("DanMaklen", domain.Id);
             if (userStat is not null) TestUserStat(domain, userStat.Value);
+            Console.WriteLine($"Testing {domain.Id} ({domain.Name})... Passed!");
         }
         Console.Out.WriteLine("All Domains passed the test!");
     }
 
     private static void TestDomain(Domain domain)
     {
+        foreach (var c in domain.Classes)
+        {
+            foreach (var sid in c.Stats)
+                if (sid.Domain.FullyQualifiedId != domain.Id.FullyQualifiedId)
+                    throw new TestFailureException(domain, $"class {c.Id} has a stat ({sid}) from an external domain");
+            foreach (var tid in c.Tags)
+                if (tid.Domain.FullyQualifiedId != domain.Id.FullyQualifiedId)
+                    throw new TestFailureException(domain, $"class {c.Id} has a tag ({tid}) from an external domain");
+        }
+
+        foreach (var e in domain.Entities)
+        {
+            if(e.Class.Domain.FullyQualifiedId != domain.Id.FullyQualifiedId)
+                throw new TestFailureException(domain, $"entity {e.Id} has a class ({e.Class}) from an external domain");
+            foreach (var sid in e.Stats)
+                if (sid.Domain.FullyQualifiedId != domain.Id.FullyQualifiedId)
+                    throw new TestFailureException(domain, $"entity {e.Id} has a stat ({sid}) from an external domain");
+            foreach (var tid in e.Tags)
+                if (tid.Domain.FullyQualifiedId != domain.Id.FullyQualifiedId)
+                    throw new TestFailureException(domain, $"entity {e.Id} has a tag ({tid}) from an external domain");
+        }
+
+        foreach (var p in domain.Projects)
+        {
+            foreach (var o in p.Objectives)
+            {
+                foreach (var r in o.Requirements)
+                {
+                    if(r.Class.Domain.FullyQualifiedId != domain.Id.FullyQualifiedId)
+                        throw new TestFailureException(domain, $"Project Objective Requirement {p.Id}.{o.Title} has a class ({r.Class}) from an external domain");
+                    if(r.Stat.Domain.FullyQualifiedId != domain.Id.FullyQualifiedId)
+                        throw new TestFailureException(domain, $"Project Objective Requirement {p.Id}.{o.Title} has a stat ({r.Stat}) from an external domain");
+                }
+            }
+        }
     }
 
     private static void TestUserStat(Domain domain, UserStats userStats)
     {
+        foreach (var es in userStats.EntityStats)
+        {
+            if(es.EntityId.Domain.FullyQualifiedId != domain.Id.FullyQualifiedId)
+                throw new TestFailureException(domain, $"Entity Stat {es.EntityId} - {es.StatId} has an entity ({es.EntityId}) from an external domain");
+            if(es.StatId.Domain.FullyQualifiedId != domain.Id.FullyQualifiedId)
+                throw new TestFailureException(domain, $"Entity Stat {es.EntityId} - {es.StatId} has a stat ({es.StatId}) from an external domain");
+        }
     }
 
     private static async IAsyncEnumerable<Domain> GetAllDomains(IDomainRepo domainRepo)
