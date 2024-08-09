981ea03ec30cb4e3fa76b6e00ffd213f73d7ca47
Omar Nasr <danmaklen@gmail.com>
2024-08-09 07:00:15 +0300

    Add Test Command (#90)

---
+++ DomainData/TestCommand.cs
@@ -0,0 +1,38 @@
+using Cocona;
+using Pika.Model;
+using Pika.Repository;
+
+namespace Pika.DomainData;
+
+public static class TestCommand
+{
+    private class TestFailureException(Domain domain, string reason) : Exception($"Domain {domain.Id} ({domain.Name}) failed a test: {reason}");
+
+    public static void AddTestCommand(this CoconaApp app)
+    {
+        app.AddCommand("test", Test);
+    }
+
+    private static async Task Test(IDomainRepo domainRepo)
+    {
+        await foreach (var domain in GetAllDomains(domainRepo))
+        {
+            TestDomain(domain);
+        }
+        Console.Out.WriteLine("All Domains passed the test!");
+    }
+
+    private static void TestDomain(Domain domain)
+    {
+        if (domain.SubDomains.Count > 0) throw new TestFailureException(domain, "TestDomainHasNoSubDomains");
+    }
+
+    private static async IAsyncEnumerable<Domain> GetAllDomains(IDomainRepo domainRepo)
+    {
+        var domains = await domainRepo.GetAll();
+        foreach (var d in domains)
+        {
+            yield return (await domainRepo.Get(d.Id.FullyQualifiedId))!;
+        }
+    }
+}
