ee59ea88285912bf95af86a6b767ed4c835b57d8
Dan Maklen <danmaklen@gmail.com>
2024-08-10 00:14:25 +0300

    Update Hades

--- DomainData/SyncCommand.cs
+++ DomainData/SyncCommand.cs
@@ -1,5 +1,3 @@
-using System.Text.Json;
-using System.Text.Json.Serialization;
 using Cocona;
 using Pika.Model;
 using Pika.PikaLang;
@@ -16,54 +14,25 @@ public static class SyncCommand
 
     private static async Task Sync([Argument] string domainId, PikaParser parser, IDomainRepo domainRepo, IEnumerable<IScrapper> scrappers)
     {
-        var domains = ReadAllPikaDomainFiles(parser, domainId);
-        var domain = domains.Single(); // TODO: Support multiple pika files
-
-        await RunAllScrapersAndUpdateDomain(domain, scrappers);
-
+        var domain = ReadPikaFile(parser, domainId);
+        await RunScrapersAndUpdateDomain(domain, scrappers);
         await domainRepo.Create(domain);
-        // DumpDomain((await domainRepo.Get(domain.Id))!);
     }
 
-    private static List<Domain> ReadAllPikaDomainFiles(PikaParser parser, string domainId)
+    private static Domain ReadPikaFile(PikaParser parser, string domainId)
     {
-        List<Domain> domains = [];
-        foreach (var file in Directory.EnumerateFiles($"Domains/{domainId}", "*.pika", SearchOption.AllDirectories))
-        {
-            Console.WriteLine($"Parsing File {file}...");
-            TextReader stream = new StreamReader(file);
-            var d = parser.Parse(stream);
-            domains.Add(d);
-        }
-
-        return domains;
+        var file = $"Domains/{domainId}.pika";
+        Console.WriteLine($"Parsing File {file}...");
+        TextReader stream = new StreamReader(file);
+        return parser.Parse(stream);
     }
 
-    private static async Task RunAllScrapersAndUpdateDomain(Domain domain, IEnumerable<IScrapper> scrappers)
+    private static async Task RunScrapersAndUpdateDomain(Domain domain, IEnumerable<IScrapper> scrappers)
     {
         foreach (var scrapper in scrappers.Where(s => s.DomainId == domain.Id))
         {
             Console.WriteLine($"Scrapping {scrapper.DomainId}...");
             await scrapper.ScrapeInto(domain);
         }
-    }
-
-    // Useful for noticing unintended changes. This dump is not used anywhere else.
-    // The dump is pushed into VCS to simplify checking for diffs in IDE. It can be mostly ignored if the diff looks satisfying.
-    private static void DumpDomain(Domain domain)
-    {
-        var filePath = $"DomainDump/{domain.Id}.dump.json";
-        Console.WriteLine($"Dumping {domain.Id} ({domain.Name}) into {filePath}...");
-
-        domain.Classes.Sort((a, b) => a.Id.ToString().CompareTo(b.Id));
-        domain.Entities.Sort((a, b) => a.Id.ToString().CompareTo(b.Id));
-        domain.Projects.Sort((a, b) => a.Name.ToString().CompareTo(b.Name));
-
-        var json = JsonSerializer.Serialize(domain, new JsonSerializerOptions
-        {
-            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
-            WriteIndented = true,
-        });
-        File.WriteAllText(filePath, json);
     }
 }
