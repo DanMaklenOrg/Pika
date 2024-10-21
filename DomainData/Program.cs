c671b007267d697b4d18f4babda7e4c3728eca0e
Dan Maklen <danmaklen@gmail.com>
2024-10-21 09:57:09 +0300

    Pika Data: Dwarfs!?

--- DomainData/Program.cs
+++ DomainData/Program.cs
@@ -27,6 +27,7 @@ builder.Services.AddTransient<IScrapper, CrossCellsScrapper>();
 builder.Services.AddTransient<IScrapper, SquareCellsScrapper>();
 builder.Services.AddTransient<IScrapper, SpecOpsTheLineScrapper>();
 builder.Services.AddTransient<IScrapper, FactorioScrapper>();
+builder.Services.AddTransient<IScrapper, DwarfsScrapper>();
 
 builder.Services.AddTransient<SteamClient>();
 builder.Services.AddTransient<SteamScrapperHelper>();
