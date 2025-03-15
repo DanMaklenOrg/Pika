c0a466d3b43697c28b262fa30ceed398a33a5b99
Dan Maklen <danmaklen@gmail.com>
2025-03-15 09:09:17 +0200

    Pika Data: Middle Earth: Shadow of Mordor

--- DomainData/DomainScrapper/MiddleEarthShadowOfMordorScrapper.cs
+++ DomainData/DomainScrapper/MiddleEarthShadowOfMordorScrapper.cs
@@ -1,3 +1,4 @@
+using HtmlAgilityPack;
 using Pika.DomainData.ScrapperHelpers;
 using Pika.Model;
 
@@ -12,5 +13,44 @@ public class MiddleEarthShadowOfMordorScrapper(SteamScrapperHelper steamScrapper
     public async Task ScrapeInto(Domain domain)
     {
         domain.Entities.AddRange(await steamScrapperHelper.ScrapAchievements(SteamAppId, "achievement"));
+        domain.Entities.AddRange(await ScrapeRunes());
+        domain.Entities.AddRange(await ScrapeAbilities());
+        domain.Entities.AddRange(await ScrapeTutorialsAndHints());
+    }
+
+    private async Task<List<Entity>> ScrapeRunes()
+    {
+        var doc = await new HtmlWeb().LoadFromWebAsync("https://shadowofwar.fandom.com/wiki/Runes");
+        var nodes = doc.DocumentNode.SelectNodes("//tr/td[2]/a");
+        return nodes.Select(n =>
+        {
+            var name = ScrapperHelper.CleanName(n.InnerText);
+            var id = ScrapperHelper.InduceIdFromName(name, "rune");
+            return new Entity(id, name, "rune");
+        }).ToList();
+    }
+
+    private async Task<List<Entity>> ScrapeAbilities()
+    {
+        var doc = await new HtmlWeb().LoadFromWebAsync("https://shadowofwar.fandom.com/wiki/Abilities");
+        var nodes = doc.DocumentNode.SelectNodes("//li/b/a");
+        return nodes.Select(n =>
+        {
+            var name = ScrapperHelper.CleanName(n.InnerText);
+            var id = ScrapperHelper.InduceIdFromName(name, "ability");
+            return new Entity(id, name, "ability");
+        }).ToList();
+    }
+
+    private async Task<List<Entity>> ScrapeTutorialsAndHints()
+    {
+        var doc = await new HtmlWeb().LoadFromWebAsync("https://shadowofwar.fandom.com/wiki/Tutorials_and_Hints");
+        var nodes = doc.DocumentNode.SelectNodes("//span[@class='mw-headline']");
+        return nodes.Select(n =>
+        {
+            var name = ScrapperHelper.CleanName(n.InnerText);
+            var id = ScrapperHelper.InduceIdFromName(name, "hint");
+            return new Entity(id, name, "hint");
+        }).ToList();
     }
 }
