3066513f81752c8a7a96b33eae55d2360f8c323a
Dan Maklen <danmaklen@gmail.com>
2025-06-09 18:47:27 +0300

    fix

--- Service/Dto/GameDto.cs
+++ Service/Dto/GameDto.cs
@@ -15,10 +15,10 @@ public readonly struct GameDto
     public List<AchievementDto> Achievements { get; init; }
 
     [JsonPropertyName("tags")]
-    public List<CategoryDto> Categories { get; init; }
+    public List<CategoryDto>? Categories { get; init; }
 
     [JsonPropertyName("entities")]
-    public List<EntityDto> Entities { get; init; }
+    public List<EntityDto>? Entities { get; init; }
 }
 
 public readonly struct AchievementDto
@@ -30,7 +30,7 @@ public readonly struct AchievementDto
     public string Name { get; init; }
 
     [JsonPropertyName("objectives")]
-    public List<ObjectiveDto> Objectives { get; init; }
+    public List<ObjectiveDto>? Objectives { get; init; }
 
     [JsonPropertyName("description")]
     public string? Description { get; init; }
