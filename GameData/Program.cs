0688c37f6a4031acd6f1b1d3e9ed7c8b604e0ef5
Dan Maklen <danmaklen@gmail.com>
2025-06-14 20:35:18 +0300

    Fix Game Schema: Spec Ops: The Line

--- GameData/Program.cs
+++ GameData/Program.cs
@@ -18,7 +18,6 @@ builder.Services.AddTransient<IScrapper, HadesScrapper>();
 builder.Services.AddTransient<IScrapper, ToTheCoreScrapper>();
 builder.Services.AddTransient<IScrapper, TimeClickersScrapper>();
 builder.Services.AddTransient<IScrapper, SquareCellsScrapper>();
-builder.Services.AddTransient<IScrapper, SpecOpsTheLineScrapper>();
 builder.Services.AddTransient<IScrapper, MiddleEarthShadowOfWarScrapper>();
 
 builder.Services.AddTransient<SteamClient>();
