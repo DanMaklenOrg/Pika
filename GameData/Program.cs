b2462c212e38354ad5c6a9adc47410d2050d49cc
Dan Maklen <danmaklen@gmail.com>
2025-06-14 19:41:38 +0300

    Fix Game Schema: Hexcells Infinite

--- GameData/Program.cs
+++ GameData/Program.cs
@@ -19,7 +19,6 @@ builder.Services.AddTransient<IScrapper, HadesScrapper>();
 builder.Services.AddTransient<IScrapper, ToTheCoreScrapper>();
 builder.Services.AddTransient<IScrapper, NodebusterScrapper>();
 builder.Services.AddTransient<IScrapper, TimeClickersScrapper>();
-builder.Services.AddTransient<IScrapper, HexcellsInfiniteScrapper>();
 builder.Services.AddTransient<IScrapper, HexcellsPlusScrapper>();
 builder.Services.AddTransient<IScrapper, SquareCellsScrapper>();
 builder.Services.AddTransient<IScrapper, SpecOpsTheLineScrapper>();
