8438f9a05833763d573e12dab7002436715397ca
Dan Maklen <danmaklen@gmail.com>
2025-06-27 18:29:11 +0300

    Game Data: Middle Earth: Shadow of War

--- GameData/Program.cs
+++ GameData/Program.cs
@@ -25,7 +25,6 @@ builder.Services.AddTransient<IScrapper, HexcellsPlusScrapper>();
 builder.Services.AddTransient<IScrapper, SquareCellsScrapper>();
 builder.Services.AddTransient<IScrapper, SpecOpsTheLineScrapper>();
 builder.Services.AddTransient<IScrapper, MiddleEarthShadowOfMordorScrapper>();
-builder.Services.AddTransient<IScrapper, MiddleEarthShadowOfWarScrapper>();
 
 builder.Services.AddTransient<SteamClient>();
 builder.Services.AddTransient<SteamScrapper>();
